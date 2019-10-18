5cad2734b79dea7d71cfeb43773091fd7abb7398
Dione <dionevoss@gmail.com>
2019-10-18 17:05:32 -0300

    Stat over :c

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,10 @@
 import React, { Component, Fragment } from 'react';
 
-import AppHeader from './header/AppHeader';
-import UserList from './components/UserList';
-
 class App extends Component{
 
  render() {
    return (
-     <Fragment>
-       <AppHeader />
-       <div className="container">
-         
-       </div>
-      <div className="container">
-        <UserList />
-      </div>
-    </Fragment>
+    <h1>Hello</h1>
    );
  }
 };
