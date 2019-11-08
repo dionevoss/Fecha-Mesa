78bf3e8f87fc346286d1feb357506a1a2c31622c
Luan F.Vieira <luanfrancavieira@gmail.com>
2019-11-08 10:02:30 -0300

    Nova estrutura para o front-end com react, iniciada com tela de login

---
+++ client-luan/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import "./styles/global";
+import Routes from './services/routes';
+import Login from './pages/Login'
+
+class App extends Component {
+    render() {
+        return(
+            <Login />
+        );
+    }
+
+}
+
+export default App;
---
+++ client-luan/src/pages/Login/index.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import './style.css'
+import { makeStyles } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+        margin: theme.spacing(2),
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: 400,
+    },
+    button: {
+        margin: theme.spacing(1),
+        width: '150px',
+      },
+      input: {
+        display: 'none',
+      },
+    button: {
+    margin: theme.spacing(1),
+    },
+    input: {
+    display: 'none',
+    },
+}));
+
+export default () => { 
+    const classes = useStyles()
+
+    return (
+        <div className='main-form'>
+            <form className='form'>
+                <h1 className='title-login-register'>Login</h1>
+                <div>
+                    <TextField 
+                        type='email' 
+                        id="standard-basic" 
+                        className={classes.textField} 
+                        label="E-mail" 
+                        margin="normal"
+                     />
+                </div>
+
+                <div>
+                    <TextField 
+                        id="standard-password-input" 
+                        label="Password" 
+                        className={classes.textField} 
+                        type="password" 
+                        autoComplete="current-password" 
+                        margin="normal" 
+                    />
+                </div>
+
+                <div className='buttons'>
+                    <Button variant="contained" color="primary" className={classes.button}>
+                        Entrar
+                    </Button>
+                    <Button color="primary" className={classes.button}>
+                        Cadastrar-se
+                    </Button>
+                </div>
+            </form>
+        </div>
+    )
+}
