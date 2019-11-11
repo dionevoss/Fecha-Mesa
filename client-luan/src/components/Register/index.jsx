0596d478c3b13b51fa17a829a0a9438dc870a2ab
Luan F.Vieira <luanfrancavieira@gmail.com>
2019-11-11 19:38:13 -0300

    Verificações de 8 caracteres minimos para registro e confirmação de senha, mostrando msg de erro

--- client-luan/src/components/Register/index.jsx
+++ client-luan/src/components/Register/index.jsx
@@ -36,17 +36,28 @@ export default () => {
     const [email, setEmail] = useState('')
     const [password1, setPassword1] = useState('')
     const [password2, setPassword2] = useState('')
+    const [warning, setWarning] = useState('')
 
     const passwordVerification = async () => {
-        if(password1 === password2) {
-            try{
-                const response = await api.post('/users', {first_name: name, last_name: lastName, email: email, password: password1})
-                console.log(response)
-            } 
-            catch(err) {
-                console.log(err)
-                console.log('Não foi')
-            }
+        const lenght = password1.split('').length
+
+        if(lenght < 8) {
+            setWarning('Senha deve ter no minimo 8 caracteres')
+            return
+        }
+
+        if(password1 !== password2) {
+            setWarning('As senhas não são iguais')
+            return
+        }
+
+        try{
+            const response = await api.post('/users', {first_name: name, last_name: lastName, email: email, password: password1})
+            console.log(response)
+        } 
+        catch(err) {
+            console.log(err)
+            setWarning(err)
         }
     }
 
@@ -102,12 +113,14 @@ export default () => {
                         value={password2}
                         type='password'
                         className={classes.input}
-                        placeholder='Senha novamente'
+                        placeholder='Confirme sua senha'
                         inputProps={{
                         'aria-label': 'description',
                         }}
                         onChange={(e) => setPassword2(e.target.value)}
                     />
+
+                    <p className='warning'>{warning}</p>
 
                     <Button variant="contained" color='primary' className={classes.button} onClick={passwordVerification}>
                         <Link className='color-1'>Finalizar Cadastro</Link>
