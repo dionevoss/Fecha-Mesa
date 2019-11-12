99475693f3494a2f3cd012fb652b824f76df0023
Dione <dionevoss@gmail.com>
2019-11-12 15:31:02 -0300

    Add new routes for groups and images

--- server/start/routes.js
+++ server/start/routes.js
@@ -19,10 +19,20 @@ const Route = use('Route')
 Route.on('/').render('welcome')
 
 Route.post('/users', 'UserController.create')
-Route.get('/users/:id', 'UserController.getId')
+Route.get('/users/:id', 'UserController.show')
 Route.get('/users', 'UserController.getAll')
-Route.put('/users/:id', 'UserController.updateUser')
-Route.delete('/users/:id', 'UserController.deleteUser')
+Route.put('/users/:id', 'UserController.update')
+Route.delete('/users/:id', 'UserController.destroy')
+
+Route.post('/groups', 'GroupController.create')
+Route.get('/groups/:id', 'GroupController.show')
+Route.get('/groups', 'GroupController.getAll')
+Route.put('/groups', 'GroupController.update')
+Route.delete('/groups/:id', 'GroupController.destroy')
+
+Route.post('/images', 'ImageController.store')
+Route.get('/images/:id', 'ImageController.show')
+Route.delete('/images/:id', 'ImageController.destroy')
 
 Route.post("/auth", "AuthController.login")
 Route.put("/auth", "AuthController.updateToken")
