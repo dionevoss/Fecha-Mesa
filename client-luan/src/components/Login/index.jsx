f552ed823e5b0321142e46c7e6345a3ca9aa24df
Dione <dionevoss@gmail.com>
2019-11-27 21:18:04 -0300

    Login utilizando jwt

--- client-luan/src/components/Login/index.jsx
+++ client-luan/src/components/Login/index.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
-import {MainFormStyle, FormStyle, TextFormStyle, ButtonStyle} from './../../themes/styled'
+import React, {useState} from 'react'
+import {MainFormStyle, FormStyle, TextFormStyle, ButtonStyle} from '../../themes/styled'
+import api from '../../services/api'
+import { login } from '../../services/auth'
 import {Link} from 'react-router-dom'
 
 export default () => { 
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    const signIn = async () => {
+        try {
+            setLoading(true)
+            const response = await api.post('/auth', {email, password})
+            login(response.data.token)
+            alert('Logado')
+            
+        } catch (error) {
+            console.log('erooooooooooooo')
+            console.log(error)
+        }
+    }
+
+
     return (
         <MainFormStyle>
             <FormStyle>
+                <form onSubmit={e => {e.preventDefault(); signIn()}}>
                 <h1>LOGIN</h1>
                 <div>
                     <TextFormStyle 
@@ -13,6 +34,8 @@ export default () => {
                         id="standard-basic" 
                         label="E-mail" 
                         margin="normal"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
                     />
                 </div>
 
@@ -23,6 +46,8 @@ export default () => {
                         type="password" 
                         autoComplete="current-password" 
                         margin="normal" 
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
                     />
                 </div>
 
@@ -30,10 +55,11 @@ export default () => {
                     <ButtonStyle width='150'>
                         <Link to='/login/register'>Cadastrar-se</Link>
                     </ButtonStyle>
-                    <ButtonStyle variant="contained" color="primary" width='150' colors='f2f2f2'>
-                        <Link to='/'  >Entrar</Link>
+                    <ButtonStyle type='submit' variant="contained" color="primary" width='150' fontColor='f2f2f2'>
+                        <Link>Entrar</Link>
                     </ButtonStyle>
                 </div>
+                </form>
             </FormStyle>
         </MainFormStyle>
     )
