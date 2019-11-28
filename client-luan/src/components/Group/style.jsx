41cfad430a1c6701528d7af53da02bb7d0a63ff8
luanfv <luanfrancavieira@gmai.com>
2019-11-28 09:36:22 -0300

    View of posts

---
+++ client-luan/src/components/Group/style.jsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+import Paper from '@material-ui/core/Paper'
+
+export const PaperStyle = styled(Paper)`
+    && {
+        padding: 15px;
+        margin-bottom: 10px;
+    }
+`
+
+export const MainLoaderStyle = styled.div`
+    width: 100%;
+    height: 500px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const UserStyle = styled.p`
+    font-size: 1.2em;
+`
+
+export const DateStyle = styled.p`
+    font-size: 0.8em;
+`
+
+export const TextStyle = styled.p`
+    font-size: 1em;
+`
