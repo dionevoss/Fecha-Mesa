912efabe9e7c2391ac05700c1ef8c32442a3f605
Dione <dionevoss@gmail.com>
2019-11-30 01:33:07 -0300

    hotfix logout

--- client-luan/src/services/auth.js
+++ client-luan/src/services/auth.js
@@ -4,6 +4,6 @@ export const getToken = () => localStorage.getItem(TOKEN_KEY);
 export const login = token => {
     localStorage.setItem(TOKEN_KEY, token);
 };
-export const logout = token => {
+export const logout = () => {
     localStorage.removeItem(TOKEN_KEY);
 };
