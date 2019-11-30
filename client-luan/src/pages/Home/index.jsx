68913d283d4f595ce612497c873c8b4ed2efb635
Dione <dionevoss@gmail.com>
2019-11-30 01:30:43 -0300

    Add redirect if ser is not logged in

--- client-luan/src/pages/Home/index.jsx
+++ client-luan/src/pages/Home/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 import User from '../../components/User'
 import Menu from '../../components/Menu'
+import { isAuthenticated } from '../../services/auth'
 
 export default () => {
+
+    if (!isAuthenticated()){
+        return <Redirect to='/login' />
+    }
+
     document.body.style.background = 'white'
 
     return (
