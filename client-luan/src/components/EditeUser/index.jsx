e1d75a91e891cdd6a2163941ae8b7f57516c2415
Dione <dionevoss@gmail.com>
2019-12-01 18:37:54 -0300

    Add update logic

--- client-luan/src/components/EditeUser/index.jsx
+++ client-luan/src/components/EditeUser/index.jsx
@@ -8,6 +8,29 @@ import api from '../../services/api'
 
 export default () => {
     const [file, setFile] = useState(null)
+    const [name, setName] = useState(undefined)
+    const [lastName, setLastName] = useState(undefined)
+    const [email, setEmail] = useState(undefined)
+    const [oldPassword, setOldPassword] = useState([])
+    const [newPassword1, setNewPassword1] = useState([])
+    const [newPassword2, setNewPassword2] = useState([])
+
+    const updateUser = async () => {
+        console.log(name)
+        try {
+            await api.put('/users', {first_name: name, last_name: lastName, email})
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const updatePassword = async () => {
+        try {
+            await api.put('/users', {password: newPassword1})
+        } catch (error) {
+            
+        }
+    }
 
     const uploadImage = async () => {
         let formData = new FormData()
@@ -44,10 +67,10 @@ export default () => {
 
             <MainStyled>
                 <h2>Nome e E-mail</h2>
-                <InputStyled label='Nome' />
-                <InputStyled label='Sobre-nome' />
-                <InputStyled label='E-mail' />
-                <ButtonStyle variant="contained" color="primary" width='300'>
+                <InputStyled value={name} onChange={(e) => setName(e.target.value)} label='Nome' />
+                <InputStyled value={lastName} onChange={(e) => setLastName(e.target.value)} label='Sobrenome' />
+                <InputStyled value={email} onChange={(e) => setEmail(e.target.value)} label='E-mail' />
+                <ButtonStyle onClick={updateUser} variant="contained" color="primary" width='300'>
                     Alterar
                 </ButtonStyle>
             </MainStyled>
@@ -56,10 +79,10 @@ export default () => {
 
             <MainStyled>
                 <h2>Senha</h2>
-                <InputStyled label='Senha atual' />
-                <InputStyled label='Nova senha' />
-                <InputStyled label='Confirme a senha' />
-                <ButtonStyle variant="contained" color="primary" width='300'>
+                <InputStyled value={oldPassword} onChange={(e) => setOldPassword(e.target.value)} label='Senha atual' />
+                <InputStyled value={newPassword1} onChange={(e) => setNewPassword1(e.target.value)} label='Nova senha' />
+                <InputStyled value={newPassword2} onChange={(e) => setNewPassword2(e.target.value)} label='Confirme a senha' />
+                <ButtonStyle onClick={updatePassword} variant="contained" color="primary" width='300'>
                     Alterar
                 </ButtonStyle>
             </MainStyled>
