902e6958cd1af51f538c2658bf7f68b584d8f776
Luan F.Vieira <luanfrancavieira@gmail.com>
2019-12-02 19:03:21 -0300

    Update edite user

--- client-luan/src/components/EditeUser/index.jsx
+++ client-luan/src/components/EditeUser/index.jsx
@@ -11,15 +11,14 @@ export default () => {
     const [name, setName] = useState(undefined)
     const [lastName, setLastName] = useState(undefined)
     const [email, setEmail] = useState(undefined)
-    const [newPassword1, setNewPassword1] = useState([])
-    const [newPassword2, setNewPassword2] = useState([])
-    const [user, setUser] = useState()
+    const [newPassword1, setNewPassword1] = useState('')
+    const [newPassword2, setNewPassword2] = useState('')
+    const [isError, setIsError] = useState(false)
 
     const fetchData = async () => {
         await api
             .get(`/users`)
             .then(result =>{
-                setUser(result.data)
                 setName(result.data.first_name)
                 setLastName(result.data.last_name)
                 setEmail(result.data.email)
@@ -30,6 +29,7 @@ export default () => {
         console.log(name)
         try {
             await api.put('/users', {first_name: name, last_name: lastName, email})
+            alert('Informações do usuário atualizadas com sucesso')
         } catch (error) {
             console.log(error)
         }
@@ -38,8 +38,10 @@ export default () => {
     const updatePassword = async () => {
         try {
             await api.put('/users', {password: newPassword1})
+            setIsError(false)
+            alert('Senha alterada com sucesso')
         } catch (error) {
-            
+            console.log(error)
         }
     }
 
@@ -56,6 +58,17 @@ export default () => {
         } catch (error) {
             console.log(error)
         }
+    }
+
+    const passwordVerification = () => {
+        const lenght = newPassword1.split('').length
+
+        if(lenght < 8 || newPassword1 !== newPassword2) {
+            setIsError(true)
+            return
+        }
+
+        updatePassword()
     }
 
     useEffect(() => {
@@ -78,7 +91,7 @@ export default () => {
             </ImageStyled>
 
             <AlignStyled>
-                <ButtonStyle onClick={uploadImage} variant="contained" color="primary" width='300'>
+                <ButtonStyle onClick={() => uploadImage()} variant="contained" color="primary" width='300'>
                     upload
                 </ButtonStyle>
             </AlignStyled>
@@ -88,7 +101,7 @@ export default () => {
                 <InputStyled value={name} onChange={(e) => setName(e.target.value)} label='Nome' />
                 <InputStyled value={lastName} onChange={(e) => setLastName(e.target.value)} label='Sobrenome' />
                 <InputStyled value={email} onChange={(e) => setEmail(e.target.value)} label='E-mail' />
-                <ButtonStyle onClick={updateUser} variant="contained" color="primary" width='300'>
+                <ButtonStyle onClick={() => updateUser()} variant="contained" color="primary" width='300'>
                     Alterar
                 </ButtonStyle>
             </MainStyled>
@@ -97,9 +110,9 @@ export default () => {
 
             <MainStyled>
                 <h2>Senha</h2>
-                <InputStyled value={newPassword1} onChange={(e) => setNewPassword1(e.target.value)} label='Nova senha' />
-                <InputStyled value={newPassword2} onChange={(e) => setNewPassword2(e.target.value)} label='Confirme a senha' />
-                <ButtonStyle onClick={updatePassword} variant="contained" color="primary" width='300'>
+                <InputStyled error={isError} type='password' value={newPassword1} onChange={(e) => setNewPassword1(e.target.value)} label='Nova senha' />
+                <InputStyled error={isError} type='password' value={newPassword2} onChange={(e) => setNewPassword2(e.target.value)} label='Confirme a senha' />
+                <ButtonStyle onClick={() => passwordVerification()} variant="contained" color="primary" width='300'>
                     Alterar
                 </ButtonStyle>
             </MainStyled>
